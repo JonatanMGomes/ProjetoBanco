43e939281543815e3279652d6ebde91ffab2ccb4
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-04-13 23:41:36 -0300

    Realizando teste numero 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProjetoBanco.Models;
+namespace ProjetoBanco
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var contaPoup = new ContaPoupanca();
+            var contaCorre = new ContaCorrente();
+            Console.WriteLine("Conta Poupança:");
+            contaPoup.ExibirSaldo();
+            Console.WriteLine("Conta Corrente:");
+            contaCorre.ExibirSaldo();
+
+            contaPoup.Depositar(550);
+            contaCorre.Depositar(420);
+            Console.WriteLine("Conta Poupança:");
+            contaPoup.ExibirSaldo();
+            Console.WriteLine("Conta Corrente:");
+            contaCorre.ExibirSaldo();
+
+            contaPoup.Sacar(300);
+            contaCorre.Sacar(150);
+            Console.WriteLine("Conta Poupança:");
+            contaPoup.ExibirSaldo();
+            Console.WriteLine("Conta Corrente:");
+            contaCorre.ExibirSaldo();
+        }
+    }
+}
